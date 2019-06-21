bf3d11c9b6edd5c2679a16fd8ca804451e52749b
monkog <monkog@wp.pl>
2019-06-21 20:03:11 +0200

    Renaming to match C# convention

--- ConwaysGameOfLife/MainWindow.xaml.cs
+++ ConwaysGameOfLife/MainWindow.xaml.cs
@@ -12,108 +12,99 @@ namespace ConwaysGameOfLife
     /// </summary>
     public partial class MainWindow : Window
     {
-        Rectangle[,] rectangleTab;
-        DispatcherTimer timer = new DispatcherTimer();
+	    private Rectangle[,] _rectangleTab;
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
-            timer.Tick += timer_Tick;
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 80);
-            is_game_running.Visibility = Visibility.Hidden;
+            _timer.Tick += TimerTick;
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 80);
+            IsGameRunning.Visibility = Visibility.Hidden;
         }
 
-        void timer_Tick(object sender, EventArgs e)
+        private void TimerTick(object sender, EventArgs e)
         {
-            check();
-            for (int i = 0; i < rectangleTab.GetLength(0); i++)
-                for (int j = 0; j < rectangleTab.GetLength(0); j++)
+            Check();
+            for (var i = 0; i < _rectangleTab.GetLength(0); i++)
+                for (var j = 0; j < _rectangleTab.GetLength(0); j++)
                 {
-                    Rectangle rect = rectangleTab[i, j];
-                    rectTag rt = (rectTag)rect.Tag;
-                    rt.color = rt.new_color;
+                    var rect = _rectangleTab[i, j];
+                    var rt = (RectTag)rect.Tag;
+                    rt.Color = rt.NewColor;
 
-                    SolidColorBrush brush;
-
-                    if (rt.color == 1)
-                        brush = new SolidColorBrush(Colors.Blue);
-                    else
-                        brush = new SolidColorBrush(SystemColors.ControlColor);
+                    var brush = rt.Color == 1 ? new SolidColorBrush(Colors.Blue) : new SolidColorBrush(SystemColors.ControlColor);
                     rect.Fill = brush;
                 }
         }
 
-        void check()
+        private void Check()
         {
-            for (int i = 0; i < rectangleTab.GetLength(0); i++)
+            for (var i = 0; i < _rectangleTab.GetLength(0); i++)
             {
-                for (int j = 0; j < rectangleTab.GetLength(0); j++)
+                for (var j = 0; j < _rectangleTab.GetLength(0); j++)
                 {
-                    rectTag rt = (rectTag)rectangleTab[i, j].Tag;
+                    var rt = (RectTag)_rectangleTab[i, j].Tag;
 
-                    int dziki_dzik = 0;
+                    var dziki_dzik = 0;
 
-                    if (i + 1 < rectangleTab.GetLength(0))
+                    if (i + 1 < _rectangleTab.GetLength(0))
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i + 1, j].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i + 1, j].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
-                    if (i + 1 < rectangleTab.GetLength(0) && j + 1 < rectangleTab.GetLength(0))
+                    if (i + 1 < _rectangleTab.GetLength(0) && j + 1 < _rectangleTab.GetLength(0))
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i + 1, j + 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i + 1, j + 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
-                    if (j + 1 < rectangleTab.GetLength(0))
+                    if (j + 1 < _rectangleTab.GetLength(0))
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i, j + 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i, j + 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
-                    if (i - 1 >= 0 && j + 1 < rectangleTab.GetLength(0))
+                    if (i - 1 >= 0 && j + 1 < _rectangleTab.GetLength(0))
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i - 1, j + 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i - 1, j + 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
                     if (i - 1 >= 0)
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i - 1, j].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i - 1, j].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
                     if (j - 1 >= 0 && i - 1 >= 0)
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i - 1, j - 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i - 1, j - 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
                     if (j - 1 >= 0)
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i, j - 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i, j - 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
 
-                    if (i + 1 < rectangleTab.GetLength(0) && j - 1 >= 0)
+                    if (i + 1 < _rectangleTab.GetLength(0) && j - 1 >= 0)
                     {
-                        rectTag tmp = (rectTag)rectangleTab[i + 1, j - 1].Tag;
-                        if (tmp.color == 1) dziki_dzik++;
+                        var tmp = (RectTag)_rectangleTab[i + 1, j - 1].Tag;
+                        if (tmp.Color == 1) dziki_dzik++;
                     }
-                    if (rt.color == 0)
+                    if (rt.Color == 0)
                     {
-
-                        if (dziki_dzik == 3)
-                            rt.new_color = 1;
-                        else
-                            rt.new_color = 0;
+	                    rt.NewColor = dziki_dzik == 3 ? 1 : 0;
                     }
                     else
                     {
                         if (dziki_dzik < 2 || dziki_dzik > 3)
-                            rt.new_color = 0;
+                            rt.NewColor = 0;
                         else
-                            rt.new_color = 1;
+                            rt.NewColor = 1;
 
                     }
                 }
@@ -121,96 +112,83 @@ namespace ConwaysGameOfLife
 
         }
 
-        void draw(int howMany)
+        private void Draw(int howMany)
         {
-            uniform_grid.Children.Clear();
-            uniform_grid.Columns = howMany;
-            uniform_grid.Rows = howMany;
+            UniformGrid.Children.Clear();
+            UniformGrid.Columns = howMany;
+            UniformGrid.Rows = howMany;
 
-            rectangleTab = new Rectangle[howMany, howMany];
+            _rectangleTab = new Rectangle[howMany, howMany];
 
-            for (int i = 0; i < howMany; i++)
-                for (int j = 0; j < howMany; j++)
+            for (var i = 0; i < howMany; i++)
+                for (var j = 0; j < howMany; j++)
                 {
-                    SolidColorBrush brush = new SolidColorBrush(SystemColors.ControlColor);
-                    Rectangle rect = new Rectangle()
+                    var brush = new SolidColorBrush(SystemColors.ControlColor);
+                    var rect = new Rectangle()
                     {
                         Fill = brush,
                         Margin = new Thickness(1, 1, 1, 1),
-                        Tag = new rectTag { color = 0, x = j, y = i, new_color = 0 }
+                        Tag = new RectTag { Color = 0, X = j, Y = i, NewColor = 0 }
                     };
-                    rectangleTab[i, j] = rect;
-                    rect.MouseDown += rect_MouseDown;
-                    uniform_grid.Children.Add(rect);
+                    _rectangleTab[i, j] = rect;
+                    rect.MouseDown += RectMouseDown;
+                    UniformGrid.Children.Add(rect);
                 }
 
         }
 
-        void rect_MouseDown(object sender, MouseButtonEventArgs e)
+        private static void RectMouseDown(object sender, MouseButtonEventArgs e)
         {
-            Rectangle rect = sender as Rectangle;
-            SolidColorBrush brush;
-            rectTag rt = (rectTag)rect.Tag;
-            if (rt.color == 1)
-                brush = new SolidColorBrush(SystemColors.ControlColor);
-            else
-                brush = new SolidColorBrush(Colors.Blue);
-            rt.color = (rt.color + 1) % 2;
+            var rect = sender as Rectangle;
+            var rt = (RectTag)rect.Tag;
+            var brush = rt.Color == 1 ? new SolidColorBrush(SystemColors.ControlColor) : new SolidColorBrush(Colors.Blue);
+            rt.Color = (rt.Color + 1) % 2;
             rect.Fill = brush;
 
         }
 
-        private void Valueslider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void ValueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            int howMany = (int)Valueslider.Value;
-            draw(howMany);
+            var howMany = (int)ValueSlider.Value;
+            Draw(howMany);
         }
 
-        public class rectTag
+        public class RectTag
         {
-            public int color { get; set; }
-            public int x { get; set; }
-            public int y { get; set; }
-            public int new_color { get; set; }
+            public int Color { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int NewColor { get; set; }
         }
 
-        private void class_version_Click(object sender, RoutedEventArgs e)
+        private void ClassVersionClick(object sender, RoutedEventArgs e)
         {
-            if (class_version.IsChecked == true)
-
-                is_game_running.Visibility = Visibility.Hidden;
-            else
-                is_game_running.Visibility = Visibility.Visible;
+	        IsGameRunning.Visibility = ClassVersion.IsChecked == true ? Visibility.Hidden : Visibility.Visible;
         }
 
-        private void is_game_running_Checked(object sender, RoutedEventArgs e)
+        private void IsGameRunningChecked(object sender, RoutedEventArgs e)
         {
-            timer.Start();
+            _timer.Start();
         }
 
-        private void is_game_running_Unchecked(object sender, RoutedEventArgs e)
+        private void IsGameRunningUnchecked(object sender, RoutedEventArgs e)
         {
-            timer.Stop();
+            _timer.Stop();
         }
 
-        private void class_version_Checked(object sender, RoutedEventArgs e)
+        private void ClassVersionChecked(object sender, RoutedEventArgs e)
         {
-            class_version.Margin = new Thickness(2, 0, 401, 8);
-            Valueslider.Margin = new Thickness(125, 0, 0, 8);
-            Value_label.Margin = new Thickness(100, 0, 0, 2);
+            ClassVersion.Margin = new Thickness(2, 0, 401, 8);
+            ValueSlider.Margin = new Thickness(125, 0, 0, 8);
+            ValueLabel.Margin = new Thickness(100, 0, 0, 2);
         }
 
-        private void class_version_Unchecked(object sender, RoutedEventArgs e)
+        private void ClassVersionUnchecked(object sender, RoutedEventArgs e)
         {
-            class_version.Margin = new Thickness(125, 0, 297, 8);
-            is_game_running.Margin = new Thickness(2, 0, 401, 8);
-            Valueslider.Margin = new Thickness(250, 0, 0, 8);
-            Value_label.Margin = new Thickness(220, 0, 0, 2);
+            ClassVersion.Margin = new Thickness(125, 0, 297, 8);
+            IsGameRunning.Margin = new Thickness(2, 0, 401, 8);
+            ValueSlider.Margin = new Thickness(250, 0, 0, 8);
+            ValueLabel.Margin = new Thickness(220, 0, 0, 2);
         }
     }
 }
-
-
-
-
-
