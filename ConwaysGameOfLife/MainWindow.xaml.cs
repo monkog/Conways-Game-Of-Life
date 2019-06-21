11233fa67017f212075623777304c40537920029
monkog <monkog@wp.pl>
2019-06-21 19:55:55 +0200

    Adapt namespaces

--- ConwaysGameOfLife/MainWindow.xaml.cs
+++ ConwaysGameOfLife/MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.ComponentModel;
 using System.Windows.Threading;
-using System.Collections;
-using System.Globalization;
 
-namespace WpfApplication2
+namespace ConwaysGameOfLife
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
